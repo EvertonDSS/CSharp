a9e1a9fd7e28acb80b2fac6ebbc549d2d2fee98f
Everton David <everton.097@gmail.com>
2022-02-24 15:50:33 -0300

    Exercicio Terreno

---
+++ condicional/terreno/terreno/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace terreno {
+    internal class Program {
+        static void Main(string[] args) {
+           CultureInfo ci = CultureInfo.InvariantCulture;
+            double largura, comprimento, area, valor, preco;
+            Console.Write("Digite a largura do Terreno: ");
+            largura = double.Parse(Console.ReadLine(),ci);
+            Console.Write("Digite o comprimento do terreno: ");
+            comprimento = double.Parse(Console.ReadLine(),ci);
+            Console.Write("Digite o valor do metro quadrado: ");
+            valor = double.Parse(Console.ReadLine(),ci);
+
+            area = largura * comprimento;
+            preco = area * valor;
+            Console.WriteLine("Area do terreno = " + area.ToString("F2", ci));
+            Console.WriteLine("Preco do terreno = " + preco.ToString("F2", ci));
+
+        }
+    }
+}
