c1a77a153d374e1a73c1cc8be7a9580d5d5947ff
EvertonDSS <everton.097@gmail.com>
2022-02-25 13:41:20 -0300

    exercicios

---
+++ exercicios/estrutura repetitiva/combustivel/combustivel/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace combustivel
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int alcool, gasolina, diesel, opcao;
+
+            alcool = 0;
+            gasolina = 0;
+            diesel = 0;
+            
+            do {
+                Console.Write("Informe um código (1, 2, 3) ou 4 para parar: ");
+                opcao = int.Parse(Console.ReadLine());
+
+                if (opcao == 1)
+                {
+                    alcool += 1;
+                }
+                else if (opcao == 2)
+                {
+                    gasolina += 1;
+                }
+                else if (opcao == 3)
+                {
+                    diesel += 1;
+                }
+            } while (opcao == 4);
+
+            Console.WriteLine("Muito obrigado!");
+            Console.WriteLine("Alcool: " + alcool);
+            Console.WriteLine("Gasolina: " + gasolina);
+            Console.WriteLine("Diesel: " + diesel);
+        }
+    }
+}
---
+++ exercicios/estrutura repetitiva/senha-fixa/senha-fixa/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace senha_fixa
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            int tentativa, senha;
+
+            senha = 2312;
+            
+            Console.Write("Digite a senha: ");
+            tentativa = int.Parse(Console.ReadLine());
+
+            while (tentativa != senha)
+            {
+                Console.Write("Senha invalida! Tente novamente: ");
+                tentativa = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Acesso permitido!");
+        }
+    }
+}
