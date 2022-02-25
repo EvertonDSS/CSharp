ea073dfa9931e6804781962b88b9575fc2fb1725
EvertonDSS <everton.097@gmail.com>
2022-02-25 10:21:15 -0300

    Atividades

---
+++ condicional/Troco/Troco/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Xml.Schema;
+
+namespace Troco
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            CultureInfo ci = CultureInfo.InvariantCulture;
+            int quantidade;
+            double valorUnidade, valorPago, troco;
+
+            Console.Write("Preço unitario do produto: ");
+            valorUnidade = double.Parse(Console.ReadLine(),ci);
+            Console.Write("Quantidade comprada: ");
+            quantidade = int.Parse(Console.ReadLine());
+            Console.Write("Dinheiro recebido: ");
+            valorPago = double.Parse(Console.ReadLine(), ci);
+            troco = valorPago - (valorUnidade * quantidade);
+            if (troco >= 0)
+            {
+                Console.WriteLine("TROCO = " + troco.ToString("F2", ci));
+            }
+            else
+            {
+                Console.WriteLine("SEM TROCO");
+            }
+        }
+    }
+}
---
+++ condicional/duracao/duracao/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace duracao
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            int tempo, hora,minuto,segundo,resto;
+
+            Console.Write("Digite a duração em segundos: ");
+            tempo = int.Parse(Console.ReadLine());
+
+            hora = tempo / 3600;
+            resto = tempo % 3600;
+
+            minuto = resto / 60;
+            segundo = resto % 60;
+            
+            Console.WriteLine(hora + ":" + minuto +":" + segundo);
+            
+        }
+    }
+}
